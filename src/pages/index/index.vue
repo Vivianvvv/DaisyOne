213510a0af2c577e0aa48fd6b81e9d6031d55212
wallace <wallacegibbon@outlook.com>
2019-10-30 17:50:17 +0800

    .

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,42 +1,101 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
 		<view>
-			<text class="title">{{title}}</text>
+			<view class="image-holder">
+				<image class="head-image" src="/static/daisy.jpg" />
+				<view class="image-gap" />
+				<image class="head-image" src="/static/wallace.jpg" />
+			</view>
+			<view class="title-holder">
+				<view class="title" v-for="(t, idx) in title" :key="idx">{{t}}</view>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
+export default {
+	data()
+	{
+		return {
+			title: [ '~ Daisy & Wallace ~' ],
 		}
+	},
+
+	onLoad()
+	{
+		setTimeout(() => {
+			this.title = [ ...this.title, 'is here' ]
+		}, 1000)
+	},
+
+	methods:
+	{
+
 	}
+}
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
+.content {
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	/* align-items: center; */
+	margin-top: 400upx;
+	justify-content: center;
+}
 
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
+.image-holder {
+	display: flex;
+	flex-direction: row;
+	margin-top: 200upx auto;
+}
 
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+.image-gap {
+	width: 750upx;
+	animation: small 1s ease-out;
+	animation-fill-mode: forwards;
+}
+
+.head-image {
+	height: 200upx;
+	width: 200upx;
+	border-radius: 100upx;
+}
+
+.title-holder {
+	position: relative;
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	padding: 40upx;
+}
+
+.title {
+	text-align: center;
+	font-size: 36upx;
+	color: #8f8f94;
+	animation: fadeshow 1.5s;
+	animation-fill-mode: forwards;
+}
+
+@keyframes small {
+	from {
+		width: 750upx;
 	}
+	to {
+		width: 40upx;
+	}
+}
+
+@keyframes fadeshow {
+	from {
+		opacity: 0;
+	}
+	to {
+		opacity: 1;
+	}
+}
 </style>
